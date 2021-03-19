ccadbbc150fd99537231ad4f41f2784b1e5ed645
Mathijs Grafhorst <mathijsgrafhorst@msn.com>
2021-03-19 09:21:53 +0100

    touch control first version

---
+++ Assets/scripts/ImageController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageController : MonoBehaviour
+{
+    ImageScriptableObject loadImage()
+    {
+        return null;
+    }
+
+}
